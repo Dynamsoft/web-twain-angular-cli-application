f6f3866538421519ffff21714e13b7cf3ba57c6b
Ellie-dynamsoft <Ellie@dynamsoft.com>
2024-01-26 09:35:29 +0800

    Update to public Key

--- src/app/dwt/dwt.component.ts
+++ src/app/dwt/dwt.component.ts
@@ -14,10 +14,18 @@ export class DwtComponent implements OnInit {
   deviceList=[];
   constructor() { }
   ngOnInit(): void {
+    Dynamsoft.OnLicenseError = function (message, errorCode) {
+      if(errorCode == -2808)
+        message = '<div style="padding:0">Sorry. Your product key has expired. You can purchase a full license at the <a target="_blank" href="https://www.dynamsoft.com/store/dynamic-web-twain/#DynamicWebTWAIN">online store</a>.</div><div style="padding:0">Or, you can try requesting a new product key at <a target="_blank" href="https://www.dynamsoft.com/customer/license/trialLicense?product=dwt&utm_source=in-product">this page</a>.</div><div style="padding:0">If you need any help, please <a target="_blank" href="https://www.dynamsoft.com/company/contact/">contact us</a>.</div>';
+      (Dynamsoft.DWT as any).ShowMessage(message, {
+        width: 680,
+        headerStyle: 2
+      });
+   };
     Dynamsoft.DWT.Containers = [{ WebTwainId: 'dwtObject', ContainerId: this.containerId, Width: '300px', Height: '400px' }];
     Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', () => { this.Dynamsoft_OnReady(); });
     Dynamsoft.DWT.ResourcesPath = '/assets/dwt-resources';
-	  Dynamsoft.DWT.ProductKey = 't0107KwEAAGoasMG9xI2Iav49dLPDNR+pjYp+ZwC2Mlvgf6RlAzGLYngM9RNg61sTaf9/OD1pJlJXhwmYhR5+GMEoOwjbuYGgASjT+QPI3FtFsOLXoefiCRicwwBsBUgYf159kSEbmSTpAaZBPWg=';
+	  Dynamsoft.DWT.ProductKey = "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9";
     Dynamsoft.DWT.Load();
   }
 
